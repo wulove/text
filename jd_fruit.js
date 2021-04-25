02b2b5f8efa32847f7828955ef35a6e0f9387811
442774765 <74840658+442774765@users.noreply.github.com>
2021-04-25 14:14:31 +0000

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -27,7 +27,7 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //助力好友分享码(最多4个,否则后面的助力失败),原因:京东农场每人每天只有四次助力机会
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes =
+//let shareCodes =
  [ // 这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
   '',
